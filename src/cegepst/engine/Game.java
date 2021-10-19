3310888c4de53943049612917020e2e675f1d32b
dadajuice <davidt2003@msn.com>
2021-10-19 09:38:49 -0400

    Refactor : séparation de la logique de jeu et de l'engin graphique

--- src/cegepst/engine/Game.java
+++ src/cegepst/engine/Game.java
@@ -11,37 +11,25 @@ public class Game {
     private static final int SLEEP = 25;
     private Ball ball;
     private boolean playing = true;
-    private JFrame frame;
-    private JPanel panel;
-    private BufferedImage bufferedImage;
-    private Graphics2D buffer;
+    private RenderingEngine renderingEngine;
     private long syncTime;
     private int score = 0;
 
     public Game() {
-        initializeFrame();
-        initializePanel();
+        renderingEngine = new RenderingEngine();
         ball = new Ball(20);
     }
 
     public void start() {
-        frame.setVisible(true);
+        renderingEngine.start();
         updateSyncTime();
-
         while (playing) {
-            bufferedImage = new BufferedImage(800, 600,
-                    BufferedImage.TYPE_INT_RGB);
-            buffer = bufferedImage.createGraphics();
-            buffer.setRenderingHints(getOptimalRenderingHints());
-
             update();
-            drawOnBuffer();
+            drawOnBuffer(renderingEngine.getRenderingBuffer());
             drawBufferOnScreen();
             sleep();
         }
-
-        frame.setVisible(false);
-        frame.dispose();
+        renderingEngine.stop();
     }
 
     public void update() {
@@ -51,7 +39,7 @@ public class Game {
         }
     }
 
-    public void drawOnBuffer() {
+    public void drawOnBuffer(Graphics2D buffer) {
         ball.draw(buffer);
 
         buffer.setPaint(Color.WHITE);
@@ -59,10 +47,7 @@ public class Game {
     }
 
     public void drawBufferOnScreen() {
-        Graphics2D graphics = (Graphics2D) panel.getGraphics();
-        graphics.drawImage(bufferedImage, 0, 0, panel);
-        Toolkit.getDefaultToolkit().sync();
-        graphics.dispose();
+        renderingEngine.renderBufferOnScreen();
     }
 
     private void sleep() {
@@ -84,34 +69,5 @@ public class Game {
             sleep = 4;
         }
         return sleep;
-    }
-
-    private RenderingHints getOptimalRenderingHints() {
-        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        rh.put(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-        return rh;
-    }
-
-    private void initializeFrame() {
-        frame = new JFrame();
-        frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null); // Center frame on screen
-        frame.setResizable(false);
-        frame.setTitle("Bouncing Balls");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setState(JFrame.NORMAL);
-
-        // Supprimer la barre de l'application
-        //setUndecorated(true);
-    }
-
-    private void initializePanel() {
-        panel = new JPanel();
-        panel.setBackground(Color.BLUE);
-        panel.setFocusable(true);
-        panel.setDoubleBuffered(true);
-        frame.add(panel); // Ajouter le panneau dans le JFrame
     }
 }
---
+++ src/cegepst/engine/RenderingEngine.java
@@ -0,0 +1,70 @@
+package cegepst.engine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class RenderingEngine {
+
+    private JFrame frame;
+    private JPanel panel;
+    private BufferedImage bufferedImage;
+
+    public RenderingEngine() {
+        initializeFrame();
+        initializePanel();
+    }
+
+    public Graphics2D getRenderingBuffer() {
+        bufferedImage = new BufferedImage(800, 600,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = bufferedImage.createGraphics();
+        graphics.setRenderingHints(getOptimalRenderingHints());
+        return graphics;
+    }
+
+    public void renderBufferOnScreen() {
+        Graphics2D graphics = (Graphics2D) panel.getGraphics();
+        graphics.drawImage(bufferedImage, 0, 0, panel);
+        Toolkit.getDefaultToolkit().sync();
+        graphics.dispose();
+    }
+
+    public void start() {
+        frame.setVisible(true);
+    }
+
+    public void stop() {
+        frame.setVisible(false);
+        frame.dispose();
+    }
+
+    private void initializeFrame() {
+        frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null); // Center frame on screen
+        frame.setResizable(false);
+        frame.setTitle("Bouncing Balls");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setState(JFrame.NORMAL);
+
+        // Supprimer la barre de l'application
+        //setUndecorated(true);
+    }
+
+    private void initializePanel() {
+        panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setFocusable(true);
+        panel.setDoubleBuffered(true);
+        frame.add(panel); // Ajouter le panneau dans le JFrame
+    }
+
+    private RenderingHints getOptimalRenderingHints() {
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        return rh;
+    }
+}
