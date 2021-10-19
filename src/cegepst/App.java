876f3b943de82dfdd5e4e9072ab04e306c8d7ea7
dadajuice <davidt2003@msn.com>
2021-10-19 09:04:57 -0400

    Refactor : renommer classe GameWindow en Game

--- src/cegepst/App.java
+++ src/cegepst/App.java
@@ -1,10 +1,10 @@
 package cegepst;
 
-import cegepst.engine.GameWindow;
+import cegepst.engine.Game;
 
 public class App {
 
     public static void main(String[] args) {
-        (new GameWindow()).start();
+        (new Game()).start();
     }
 }
