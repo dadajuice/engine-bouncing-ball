975bdf8d652fe1310ea14fdd406e8b924cd06ca6
dadajuice <davidt2003@msn.com>
2021-10-22 12:07:32 -0400

    Intégration d'une joueur

--- src/cegepst/movingRectangle/MovingRectangleGame.java
+++ src/cegepst/movingRectangle/MovingRectangleGame.java
@@ -5,19 +5,21 @@ import cegepst.engine.Game;
 
 public class MovingRectangleGame extends Game {
 
+    private Player player;
+
     @Override
     public void initialize() {
-
+        player = new Player(100, 100);
     }
 
     @Override
     public void update() {
-
+        player.update();
     }
 
     @Override
     public void draw(Buffer buffer) {
-
+        player.draw(buffer);
     }
 
     @Override
