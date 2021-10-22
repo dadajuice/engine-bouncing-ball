7ffc78c5fe63d8fe4aca20180af7ad28bed8d4ed
dadajuice <davidt2003@msn.com>
2021-10-22 11:50:14 -0400

    Ajout de statistiques pour le temps

--- src/cegepst/BouncingBallGame.java
+++ src/cegepst/BouncingBallGame.java
@@ -2,6 +2,7 @@ package cegepst;
 
 import cegepst.engine.Buffer;
 import cegepst.engine.Game;
+import cegepst.engine.GameTime;
 
 import java.awt.*;
 
@@ -27,6 +28,8 @@ public class BouncingBallGame extends Game {
     public void draw(Buffer buffer) {
         ball.draw(buffer);
         buffer.drawText("Score: " + score, 10, 20, Color.WHITE);
+        buffer.drawText("FPS: " + GameTime.getCurrentFps(), 10, 40, Color.WHITE);
+        buffer.drawText(GameTime.getElapsedFormattedTime(), 10, 60, Color.WHITE);
     }
 
     @Override
