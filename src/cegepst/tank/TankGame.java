e3e7ac1cf50027776ab32f7823b7b5c9b83b57e5
dadajuice <davidt2003@msn.com>
2021-10-29 12:19:41 -0400

    Ajout d'un tank qui peut tirer des missiles

--- src/cegepst/tank/TankGame.java
+++ src/cegepst/tank/TankGame.java
@@ -3,14 +3,21 @@ package cegepst.tank;
 import cegepst.engine.Buffer;
 import cegepst.engine.Game;
 
+import java.util.ArrayList;
+
 public class TankGame extends Game {
 
     private GamePad gamePad;
+    private Tank tank;
+    private ArrayList<Missile> missiles;
 
     @Override
     public void initialize() {
         gamePad = new GamePad();
+        tank = new Tank(gamePad);
         addKeyListener(gamePad); // TODO: Makes me puke
+
+        missiles = new ArrayList<>();
     }
 
     @Override
@@ -18,11 +25,21 @@ public class TankGame extends Game {
         if (gamePad.isQuitPressed()) {
             stop();
         }
+        tank.update();
+        if (gamePad.isFirePressed() && tank.canFire()) {
+            missiles.add(tank.fire());
+        }
+        for (Missile missile : missiles) {
+            missile.update();
+        }
     }
 
     @Override
     public void draw(Buffer buffer) {
-
+        tank.draw(buffer);
+        for (Missile missile : missiles) {
+            missile.draw(buffer);
+        }
     }
 
     @Override
