73a8908dd5a2a0fdfcaa9a0dbfa4a27de05a571a
dadajuice <davidt2003@msn.com>
2021-10-29 12:11:48 -0400

    Ajout d'un accesseur et mutateur pour la direction

--- src/cegepst/engine/entities/MovableEntity.java
+++ src/cegepst/engine/entities/MovableEntity.java
@@ -29,6 +29,14 @@ public abstract class MovableEntity extends UpdatableEntity {
         y += direction.getVelocityY(speed);
     }
 
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
     public int getSpeed() {
         return speed;
     }
