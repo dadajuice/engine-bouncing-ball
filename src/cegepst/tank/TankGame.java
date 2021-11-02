4b54d379ae240723e2cfbf3146b3c64b7711bd60
dadajuice <davidt2003@msn.com>
2021-11-02 08:24:56 -0400

    Intégration de blocs de collision

--- src/cegepst/tank/TankGame.java
+++ src/cegepst/tank/TankGame.java
@@ -10,12 +10,19 @@ public class TankGame extends Game {
     private GamePad gamePad;
     private Tank tank;
     private ArrayList<Missile> missiles;
+    private ArrayList<Brick> bricks;
 
     @Override
     public void initialize() {
         gamePad = new GamePad();
         tank = new Tank(gamePad);
         missiles = new ArrayList<>();
+        bricks = new ArrayList<>();
+        bricks.add(new Brick(500, 100));
+        bricks.add(new Brick(500, 116));
+        bricks.add(new Brick(500, 132));
+        bricks.add(new Brick(484, 148));
+        bricks.add(new Brick(500, 164));
     }
 
     @Override
@@ -37,6 +44,9 @@ public class TankGame extends Game {
         tank.draw(buffer);
         for (Missile missile : missiles) {
             missile.draw(buffer);
+        }
+        for (Brick brick : bricks) {
+            brick.draw(buffer);
         }
     }
 
