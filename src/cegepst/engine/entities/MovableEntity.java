66c5bc5ea40303ab298d22dd547aa3d1af7b39b2
dadajuice <davidt2003@msn.com>
2021-11-02 09:55:39 -0400

    Intégration du moteur de collision

--- src/cegepst/engine/entities/MovableEntity.java
+++ src/cegepst/engine/entities/MovableEntity.java
@@ -2,10 +2,26 @@ package cegepst.engine.entities;
 
 import cegepst.engine.controls.Direction;
 
+import java.awt.*;
+
 public abstract class MovableEntity extends UpdatableEntity {
 
+    private Collision collision;
     private Direction direction = Direction.UP;
     private int speed;
+    private boolean moved;
+    private int lastX;
+    private int lastY;
+
+    public MovableEntity() {
+        collision = new Collision(this);
+        speed = 1;
+    }
+
+    @Override
+    public void update() {
+        moved = false;
+    }
 
     public void moveDown() {
         move(Direction.DOWN);
@@ -25,8 +41,12 @@ public abstract class MovableEntity extends UpdatableEntity {
 
     public void move(Direction direction) {
         this.direction = direction;
-        x += direction.getVelocityX(speed);
-        y += direction.getVelocityY(speed);
+        int allowedSpeed = collision.getAllowedSpeed(direction);
+        x += direction.getVelocityX(allowedSpeed);
+        y += direction.getVelocityY(allowedSpeed);
+        moved = (x != lastX || y != lastY);
+        lastX = x;
+        lastY = y;
     }
 
     public Direction getDirection() {
@@ -43,5 +63,35 @@ public abstract class MovableEntity extends UpdatableEntity {
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    public Rectangle getHitBox() {
+        switch (direction) {
+            case UP: return getUpperHitBox();
+            case DOWN: return getLowerHitBox();
+            case LEFT: return getLeftHitBox();
+            case RIGHT: return getRightHitBox();
+            default: return getBounds();
+        }
+    }
+
+    public boolean hitBoxIntersectWith(StaticEntity other) {
+        return getHitBox().intersects(other.getBounds());
+    }
+
+    private Rectangle getLeftHitBox() {
+        return new Rectangle(x - speed, y, speed, height);
+    }
+
+    private Rectangle getRightHitBox() {
+        return new Rectangle(x + width, y, speed, height);
+    }
+
+    private Rectangle getUpperHitBox() {
+        return new Rectangle(x, y - speed, width, speed);
+    }
+
+    private Rectangle getLowerHitBox() {
+        return new Rectangle(x, y + height, width, speed);
     }
 }
